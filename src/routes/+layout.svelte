eed610fd1331db7d08e81a2ce18efe1944e6587c
Valentin <61027346+Vaalley@users.noreply.github.com>
2024-04-24 22:25:23 +0200

    Updated dependencies + added Posthog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,17 @@
 	import Navbar from '$lib/components/Navbar.svelte';
 	import { ModeWatcher } from 'mode-watcher';
 	import '../app.pcss';
+	import posthog from 'posthog-js';
+	import { browser } from '$app/environment';
+
+	export const load = async () => {
+		if (browser) {
+			posthog.init('phc_SO0k3eeW6LT7SDkFYEUCz7E90Qo7N82JIEyDe2SEWpx', {
+				api_host: 'https://eu.i.posthog.com'
+			});
+		}
+		return;
+	};
 </script>
 
 <Navbar />
